b436df2b6f8b0d431a1e6e3599e5434c7f5b6ecf
rashid <rotshild_as@mail.ru>
2019-01-25 16:46:49 +0500

    Первое сохранение

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test1.ru/admin/');
+define('HTTP_CATALOG', 'http://test1.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test1.ru/admin/');
+define('HTTPS_CATALOG', 'http://test1.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/test1.ru/public_html/admin/');
+define('DIR_SYSTEM', '/var/www/test1.ru/public_html/system/');
+define('DIR_IMAGE', '/var/www/test1.ru/public_html/image/');
+define('DIR_LANGUAGE', '/var/www/test1.ru/public_html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/test1.ru/public_html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/test1.ru/public_html/system/config/');
+define('DIR_CACHE', '/var/www/test1.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/test1.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/test1.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/test1.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/test1.ru/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/test1.ru/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'mentat');
+define('DB_DATABASE', 'rash');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test1.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test1.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/test1.ru/public_html/catalog/');
+define('DIR_SYSTEM', '/var/www/test1.ru/public_html/system/');
+define('DIR_IMAGE', '/var/www/test1.ru/public_html/image/');
+define('DIR_LANGUAGE', '/var/www/test1.ru/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/test1.ru/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/test1.ru/public_html/system/config/');
+define('DIR_CACHE', '/var/www/test1.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/test1.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/test1.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/test1.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/test1.ru/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'mentat');
+define('DB_DATABASE', 'rash');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
